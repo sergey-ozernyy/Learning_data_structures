0fb60a68994749383771c440390604ac12f3a5e2
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-11-10 21:45:37 +0300

    Добавлен негативный параметризованный тест метода equals (equalsNegativeTest)

--- src/trees/tests/EqualsTests.java
+++ src/trees/tests/EqualsTests.java
@@ -2,16 +2,20 @@ package trees.tests;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import trees.RecursiveTree;
 import trees.Tree;
 
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class EqualsTests {
     @Test
-    void equalsNull(){
+    void equalsNull() {
         // Arrange.
         Tree tree1 = createTree();
 
@@ -26,19 +30,19 @@ public class EqualsTests {
         return new int[][]{
                 {},
                 {3},
-                {3,1},
-                {3,1,0},
-                {3,1,0,2,5,4}
+                {3, 1},
+                {3, 1, 0},
+                {3, 1, 0, 2, 5, 4}
         };
     }
 
-    @MethodSource ("positiveData")
+    @MethodSource("positiveData")
     @ParameterizedTest
-    void equalsPositiveTest(int[] elements){
+    void equalsPositiveTest(int[] elements) {
         // Arrange.
         Tree tree1 = createTree();
         Tree tree2 = createTree();
-        for (var e:elements) {
+        for (var e : elements) {
             tree1.add(e);
             tree2.add(e);
         }
@@ -48,6 +52,33 @@ public class EqualsTests {
 
         // Assert.
         assertTrue(result);
+    }
+
+    static Arguments[] negativeData() {
+        return new Arguments[]{
+                arguments(new int[]{1, 2}, new int[]{3, 4}),
+                arguments(new int[]{5, 6}, new int[]{7, 8})
+        };
+    }
+
+    @MethodSource("negativeData")
+    @ParameterizedTest
+    void equalsNegativeTest(int[] elementsTree1, int[] elementsTree2) {
+        // Arrange.
+        Tree tree1 = createTree();
+        Tree tree2 = createTree();
+        for (var e : elementsTree1) {
+            tree1.add(e);
+        }
+        for (var e : elementsTree2) {
+            tree2.add(e);
+        }
+
+        // Act!
+        var result = tree1.equals(tree2);
+
+        // Assert.
+        assertFalse(result);
     }
 
     private Tree createTree() {
